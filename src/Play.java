bcda73c9995e073a8445b363545f541bb57af951
Woz56 <wozmatt@gmail.com>
2022-04-19 13:37:57 -0500

    added word list, random word selection, color coding, and started moving into OOP approach

---
+++ src/Play.java
@@ -0,0 +1,6 @@
+public class Play {
+    public static void main(String[] args) {
+        Wordle game = new Wordle();
+        game.runGame();
+    }
+}
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,8 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Wordle {
-    public static void main(String[] args) {
-        String word = "TESTS"; // will eventually pick a random word from a list of all 5-letter words
+
+    List<String> wordList;
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public Wordle() {
+        this.wordList = readInWords("five_letter_words.txt");
+    }
+
+    public List<String> readInWords(String fileName) {
+        try {
+            Scanner s = new Scanner(new File(fileName));
+            ArrayList<String> list = new ArrayList();
+            while (s.hasNext()){
+                list.add(s.next());
+            }
+            s.close();
+            return list;
+        } catch (Exception e){
+            System.out.println("Error: " + e);
+        }
+       return null;
+    }
+
+    public String getRandomWord(List<String> wordList) {
+        Random rand = new Random();
+        int int_random = rand.nextInt(wordList.size());
+        return wordList.get(int_random);
+    }
+
+    public void runGame() {
+        String word = getRandomWord(wordList).toUpperCase();
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Wordle, Guess the 5-letter word in 6 guesses");
         int guess_count = 1;
@@ -15,18 +50,19 @@ public class Wordle {
             } else {
                 for (int i = 0; i < guess.length(); i++) {
                     if (guess.charAt(i) == word.charAt(i)) {
-                        System.out.print("✓");
+                        System.out.print(ANSI_GREEN + guess.charAt(i) + ANSI_RESET);
                     } else if (word.indexOf(guess.charAt(i)) != -1) {
-                        System.out.print("C");
+                        System.out.print(ANSI_YELLOW + guess.charAt(i) + ANSI_RESET);
                     } else {
-                        System.out.print("X");
+                        System.out.print(guess.charAt(i));
                     }
                 }
             }
             guess_count++;
         }
         if (!guess.equals(word)) {
-            System.out.println("GOOD TRY, THE CORRECT WORD WAS " + word);
+            System.out.println("\n\nGOOD TRY, THE CORRECT WORD WAS " + ANSI_GREEN + word + ANSI_RESET);
         }
     }
 }
+
